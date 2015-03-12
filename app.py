c770f5ccb7051fd7c78915b82ee7780333c9808e
Stephan Klinger <staeff@arcor.de>
2015-03-12 17:44:05 +0000

    More stuff

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
+            return redirect(url_for('index',
                                     filename=filename))
 
     image_files = [f for f in os.listdir(app.config['UPLOAD_FOLDER'])]
